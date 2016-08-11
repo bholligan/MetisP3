4d0f8cec7eecc3ff138978ca4109ec757b49b87c
Brian Holligan <brian.matthew.holligan@gmail.com>
2016-08-10 20:26:38 -0700

    sliders are working now

--- predictor.py
+++ predictor.py
@@ -8,9 +8,17 @@ import pickle
 #---------- MODEL IN MEMORY ----------------#
 
 # Load the model from a pickle file
-clf = pickle.load('#filename.pkl')
-x_median = # Put array of features that are the median person
-median_prob = clf.predict_proba(x_median)[1]
+with open('diabetes_xgboost.pkl', 'rb') as f:
+    clf = pickle.load(f, encoding = 'Latin1')
+with open('diabetes_med.pkl', 'rb') as f:
+    meds = pickle.load(f, encoding = 'Latin1')
+median_prob = clf.predict_proba(meds)[0][0]
+
+def change_df(df, s, a, c):
+    df.iloc[0, 12] = s
+    df.iloc[0, 15] = a
+    df.iloc[0, 8] = c
+    return df
 
 #---------- URLS AND WEB PAGES -------------#
 
@@ -23,7 +31,8 @@ def index():
     """
     Homepage: serve our visualization page, index.html
     """
-    return render_template("index.html")
+    return render_template("index.html",
+        median_prob = median_prob)
 
 # Get an example and return its score from the predictor model
 @app.route("/score/", methods=["POST"])
@@ -35,9 +44,16 @@ def score():
     """
     # Get decision score for our example that came with the request
     data = request.json
-    x = np.matrix(data["example"])
-    person_prob = clf.predict_proba(x)[1]
-    rel_score = person_prob/median_prob
+    sugar, age, cholesterol = data[0], data[1], data[2]
+    df_u = meds.reindex_axis(sorted(meds.columns), axis = 1)
+    df_u = change_df(df_u, sugar, age, cholesterol)
+    #df = pd.read_json(data, orient = 'records')
+    #df = df.reindex_axis(sorted(df.columns), axis =1)
+    person_prob = clf.predict_proba(df_u)[0][0]
+    print("Person P: {}".format(person_prob))
+    print("Median P: {}".format(median_prob))
+    rel_score = str(person_prob/median_prob)
+    print("Rel Score: {}".format(rel_score))
     # Put the result in a nice dict so we can send it as json
     results = {"rel_score": rel_score}
     return jsonify(results)
